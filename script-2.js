776e8feceb8862decc12ddf7712bbff0228fe009
Josh <bricsio.joshua.benavides@gmail.com>
2022-03-12 15:17:46 +1000

    Completed functioning game.

--- script-2.js
+++ script-2.js
@@ -1,5 +1,3 @@
-const buttons = document.querySelectorAll("button");
-
 function test(e) {
     console.log(e.target.value);
 }
@@ -9,11 +7,25 @@ let computerPlay = () => {
     return answer[Math.floor(Math.random() * 3)];
 }
 
+let playerScore = 0,
+    computerScore = 0;
+
+let resetGame = () => {
+    document.getElementById("sub1").textContent = "Let's Play";
+    document.getElementById("sub2").textContent = "First player to score 5 points wins the game";
+    document.getElementById("player-choice").innerHTML = "&#10068";
+    document.getElementById("computer-choice").innerHTML = "&#10068";
+    document.getElementById("player-score").textContent = `Player: 0`;
+    document.getElementById("computer-score").innerText = `Computer: 0`;
+    playerScore = 0;
+    computerScore = 0;
+}
+
 let playRound = (e) => {
     let result = "",
         playerSelection = e.target.value,
         computerSelection = computerPlay();
-    switch (playerSelection.toUpperCase()) {
+    switch (playerSelection) {
         case "ROCK":
             switch (computerSelection) {
                 case "ROCK":
@@ -21,9 +33,11 @@ let playRound = (e) => {
                     break;
                 case "PAPER":
                     result = `You lose! PAPER beats ROCK.`;
+                    computerScore++;
                     break;
                 case "SCISSORS":
                     result = `You win! ROCK beats SCISSORS.`;
+                    playerScore++;
                     break;
             }
             break;
@@ -31,12 +45,14 @@ let playRound = (e) => {
             switch (computerSelection) {
                 case "ROCK":
                     result = `You win! PAPER beats ROCK.`;
+                    playerScore++;
                     break;
                 case "PAPER":
                     result = `It's a tie! The computer also picked PAPER.`;
                     break;
                 case "SCISSORS":
                     result = `You lose! SCISSORS beats PAPER.`;
+                    computerScore++;
                     break;
             }
             break;
@@ -44,9 +60,11 @@ let playRound = (e) => {
             switch (computerSelection) {
                 case "ROCK":
                     result = `You lose! ROCK beats SCISSORS.`;
+                    computerScore++;
                     break;
                 case "PAPER":
                     result = `You win! SCISSORS beats PAPER.`;
+                    playerScore++;
                     break;
                 case "SCISSORS":
                     result = `It's a tie! The computer also picked SCISSORS.`;
@@ -54,10 +72,31 @@ let playRound = (e) => {
             }
             break;
     }
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(result);
-    return result;
+
+    // Update subheadings
+    document.getElementById("sub1").textContent = result.substring(0, result.indexOf("!") + 1);
+    document.getElementById("sub2").textContent = result.substring(result.indexOf("!") + 1);
+
+
+    // Update player and computer's choices
+    playerChoice = document.querySelector(`button[value="${e.target.value}"]`).innerText;
+    computerChoice = document.querySelector(`button[value="${computerSelection}"]`).innerText;
+    document.getElementById("player-choice").textContent = playerChoice;
+    document.getElementById("computer-choice").textContent = computerChoice;
+
+    // Update score
+    document.getElementById("player-score").textContent = `You: ${playerScore}`;
+    document.getElementById("computer-score").innerText = `Computer: ${computerScore}`;
+
+    if (playerScore == 5) {
+        window.alert("Congratulaitons, you won!");
+        resetGame();
+    } else if (computerScore == 5) {
+        window.alert("You lost, good try!");
+        resetGame();
+    }
+    return;
 }
 
+const buttons = document.querySelectorAll("button");
 buttons.forEach(button => button.addEventListener('click', playRound));
